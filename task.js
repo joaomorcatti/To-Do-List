95c30884811f3cf74de0c551f6da547c131403dc
João Vitor <joamorcatti@gmail.com>
2026-02-11 19:59:22 -0300

    Inicio projeto To-Do List

---
+++ task.js
@@ -0,0 +1,70 @@
+function isValidText(text) {
+  if (text.trim() != "") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function confirmDelete() {
+  let res = window.confirm("Confirma deletar a task?");
+
+  if (res) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function deleteTask() {
+  if (confirmDelete()) {
+    console.log("Task deletada.");
+  } else {
+    console.log("Task não deletada.");
+  }
+}
+
+function task(text, taskItem) {
+  let iCheck = document.createElement("input");
+  let sText = document.createElement("span");
+  let sTag = document.createElement("span");
+  let bDel = document.createElement("button");
+
+  taskItem.className = "taskItem";
+  iCheck.className = "iCheck";
+  sText.className = "sText";
+  sTag.className = "sTag";
+  bDel.className = "bDel";
+
+  iCheck.type = "checkbox";
+  sText.textContent = `${text}`;
+  sTag.textContent = "tag";
+  bDel.textContent = "Deletar";
+
+  bDel.addEventListener("click", function () {
+    deleteTask();
+  });
+
+  taskItem.appendChild(iCheck);
+  taskItem.appendChild(sText);
+  taskItem.appendChild(sTag);
+  taskItem.appendChild(bDel);
+}
+
+function addTask() {
+  let iText = document.querySelector("input#iTask");
+  let local = document.querySelector("div#dList");
+  let taskItem = document.createElement("li");
+
+  taskText = iText.value; // Preciso declarar em uma variavel diferente, para poder limprar e usar o .focus().
+  task(taskText, taskItem);
+
+  if (isValidText(taskText)) {
+    local.appendChild(taskItem);
+  } else {
+    console.log("[Erro] Campo vazio");
+  }
+
+  iText.value = "";
+  iText.focus();
+}
